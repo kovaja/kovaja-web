cf8d4dae9d63cb79bd84f30529d23dd1f7fbaab4
Jakub Kovarik <jakubkovarik@seznam.cz>
2019-11-12 21:34:16 +0100

    Added about me component

---
+++ vue-client/src/components/home/AboutMe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid">
+    <h1>Hi, I'm Kuba</h1>
+    <p>
+      A Frontend developer, based in Prague. Suspendisse nisl. Nunc auctor. Cras
+      pede libero, dapibus nec, pretium sit amet, tempor quis. Duis ante orci,
+      molestie vitae vehicula venenatis, tincidunt ac pede. Proin mattis lacinia
+      justo. Morbi imperdiet, mauris ac auctor dictum, nisl ligula egestas
+      nulla, et sollicitudin sem purus in lacus. Maecenas aliquet accumsan leo.
+      Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Nullam
+      rhoncus aliquam metus. Praesent in mauris eu tortor porttitor accumsan.
+      Aenean placerat. Mauris dolor felis, sagittis at, luctus sed, aliquam non,
+      tellus.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "AboutMe"
+});
+</script>
--- vue-client/src/components/home/Home.vue
+++ vue-client/src/components/home/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="row align-items-center justify-content-center">
     <div class="col-sm-12 col-md-6">
-      <div style="background-color: #325fac; color: white;">
-        MAIN CONTENT
-      </div>
+      <AboutMe />
     </div>
     <div class="com-sm-12 col-md-6">
       <GithubOverview />
@@ -14,11 +12,13 @@
 <script lang="ts">
 import Vue from "vue";
 import GithubOverview from "./github-overview/GithubOverview.vue";
+import AboutMe from "./AboutMe.vue";
 
 export default Vue.extend({
   name: "Home",
   components: {
-    GithubOverview
+    GithubOverview,
+    AboutMe
   }
 });
 </script>
