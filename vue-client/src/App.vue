15412cffbd97ca9e0013a7173abc574192000a70
Jakub Kovarik <jakubkovarik@seznam.cz>
2019-11-12 20:00:28 +0100

    Added header with navigation, adjusted layout

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div id="app">
-    <GithubOverview />
+  <div id="app" class="container-fluid p-0">
+    <Header />
+    <div class="container-fluid">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-sm-12 col-md-6">
+          <div
+            style=" height: 600px; background-color: #325fac; color: white;"
+          >
+            MAIN CONTENT
+          </div>
+        </div>
+        <div class="com-sm-12 col-md-6">
+          <GithubOverview />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import GithubOverview from "./components/github-overview/GithubOverview.vue";
+import Header from "./components/Header.vue";
 
 export default Vue.extend({
   name: "app",
   components: {
-    GithubOverview
+    GithubOverview,
+    Header
   }
 });
 </script>
---
+++ vue-client/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="/">
+      <img
+        src="../assets/logo.png"
+        width="30"
+        height="30"
+        class="d-inline-block align-top"
+        alt=""
+      />
+      Kovaja
+    </a>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Projects</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+
+export default Vue.extend({
+  name: "Header"
+});
+</script>
+
+<style scoped lang="scss">
+</style>
