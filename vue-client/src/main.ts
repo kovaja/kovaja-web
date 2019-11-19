ff2555d210f3ab14f6765e558d12382ff9cc46c8
Jakub Kovarik <jakubkovarik@seznam.cz>
2019-11-19 19:53:28 +0100

    Adding state management

--- vue-client/src/main.ts
+++ vue-client/src/main.ts
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Vuex from "vuex";
 import App from "./App.vue";
 import routes from "./routing/routes";
 import "./styles/styles.scss";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 const router = new VueRouter({ routes, mode: "history" });
 
