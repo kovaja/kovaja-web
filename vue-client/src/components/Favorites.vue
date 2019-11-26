d5b03ae719fb8193bdda82c0648d99b97713838c
Jakub Kovarik <jakubkovarik@seznam.cz>
2019-11-26 22:07:41 +0100

    Default content for favorites page

--- vue-client/src/components/Favorites.vue
+++ vue-client/src/components/Favorites.vue
@@ -1,16 +1,45 @@
 <template>
-  <div class="row align-items-center justify-content-center">
-    <UnderConstruction />
+  <div class="row align-items-center">
+    <div class="col-sm-12 col-md-4">
+      <div class="p-4 shadow">
+        <h1>Articles</h1>
+        <p>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Nullam lectus justo,
+          vulputate eget mollis sed, tempor sed magna. Phasellus rhoncus.
+          Aliquam id dolor.
+        </p>
+      </div>
+    </div>
+    <div class="col-sm-12 col-md-4">
+      <div class="p-4 shadow">
+        <h1>Podcasts</h1>
+        <p>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Nullam lectus justo,
+          vulputate eget mollis sed, tempor sed magna. Phasellus rhoncus.
+          Aliquam id dolor.
+        </p>
+      </div>
+    </div>
+    <div class="col-sm-12 col-md-4">
+      <div class="p-4 shadow">
+        <h1>Sports</h1>
+        <p>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Nullam lectus justo,
+          vulputate eget mollis sed, tempor sed magna. Phasellus rhoncus.
+          Aliquam id dolor.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import UnderConstruction from "@/components/common/UnderConstruction.vue";
 
 export default Vue.extend({
-  name: "Favorites",
-  components: { UnderConstruction }
+  name: "Favorites"
 });
 </script>
-
