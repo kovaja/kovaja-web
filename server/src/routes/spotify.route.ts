8c5e274cef367f454c45a28c3f06f5cc21f59115
Jakub Kovarik <jakubkovarik@seznam.cz>
2019-12-17 23:03:34 +0100

    Download recently played tracks from spotify

--- server/src/routes/spotify.route.ts
+++ server/src/routes/spotify.route.ts
@@ -1,35 +1,36 @@
 import { Request, Response, Router } from 'express';
-import { PocketController } from '../controllers/pocket.controller';
+import { AdminController } from '../controllers/admin.controller';
+import { SpotifyController } from '../controllers/spotify.controller';
 import { Api } from '../utils/api';
 
-export class PocketRoute {
-  private controller: PocketController;
+export class SpotifyRoute {
+  private controller: SpotifyController;
 
   constructor(router: Router) {
-    this.controller = new PocketController();
+    this.controller = new SpotifyController();
 
     const subRouter = Router();
 
-    router.use('/pocket', subRouter);
+    router.use('/spotify', subRouter);
 
     subRouter.get('/login', this.handleLogin.bind(this));
     subRouter.get('/redirect', this.handleRedirect.bind(this));
-    subRouter.get('/articles', this.handleArticles.bind(this));
+    subRouter.get('/played', this.handlePlayed.bind(this));
   }
 
   private handleLogin(req: Request, res: Response): void {
-    this.controller.loginToPocket(res)
+    this.controller.loginToSpotify(res)
       .catch(Api.handleError(res));
   }
 
   private handleRedirect(req: Request, res: Response): void {
-    this.controller.handlePocketCallback()
+    this.controller.handleSpotifyCallback(req)
       .then(Api.handleResponse(res))
       .catch(Api.handleError(res));
   }
 
-  private handleArticles(req: Request, res: Response): void {
-    this.controller.getArticles()
+  private handlePlayed(req: Request, res: Response): void {
+    this.controller.getRecentlyPlayed()
       .then(Api.handleResponse(res))
       .catch(Api.handleError(res));
   }
