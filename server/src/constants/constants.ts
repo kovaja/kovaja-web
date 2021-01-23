4bb9b7432be7d649b96eb8fda60ca29894c7cfcc
Jakub Kovarik <jakubkovarik@seznam.cz>
2021-01-23 22:49:42 +0100

    Use dotenv to handle env variables, gcloud deploy

--- server/src/constants/constants.ts
+++ server/src/constants/constants.ts
@@ -1,9 +1,10 @@
-import { LocalConstants } from '../../env.local';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const Constants = {
   APP_NAME: 'kovaja-web',
-  DB_HOST: process.env.DB_HOST || LocalConstants['DB_HOST'],
-  DB_NAME: process.env.DB_NAME || LocalConstants['DB_NAME'],
-  DB_USERNAME: process.env.DB_USERNAME || LocalConstants['DB_LOGIN'],
-  DB_PWD: process.env.DB_PWD || LocalConstants['DB_PWD']
+  DB_HOST: process.env.DB_HOST,
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PWD: process.env.DB_PWD
 };
